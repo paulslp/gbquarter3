e2f8ede4760a7d1e664377ef168cd3f4471d358b
PSelyutin <pselyutin@inno.tech>
2021-09-16 11:42:11 +0400

    HW05 выполнено задание 1

--- src/main/java/gb/Car.java
+++ src/main/java/gb/Car.java
@@ -1,5 +1,10 @@
 package gb;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
 
@@ -10,6 +15,10 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier cyclicBarrierForStartRace;
+    private AtomicInteger startDeclaringCount;
+    private Semaphore semaphore;
+    private AtomicInteger finishEventCount;
 
     public String getName() {
         return name;
@@ -19,23 +28,60 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed,
+               CyclicBarrier cyclicBarrierForStartRace,
+               AtomicInteger startDeclaringCount,
+               Semaphore semaphore,
+               AtomicInteger finishEventCount) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.cyclicBarrierForStartRace = cyclicBarrierForStartRace;
+        this.startDeclaringCount = startDeclaringCount;
+        this.semaphore = semaphore;
+        this.finishEventCount = finishEventCount;
     }
 
     public void run() {
+
+        System.out.println(this.name + " готовится");
         try {
-            System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
-            System.out.println(this.name + " готов");
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println(this.name + " готов");
+        try {
+            cyclicBarrierForStartRace.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        if (startDeclaringCount.incrementAndGet() == 1) {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        }
         for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
+            Stage currentStage = race.getStages().get(i);
+            if (currentStage instanceof Tunnel) {
+                try {
+                    semaphore.acquire();
+                    race.getStages().get(i).go(this);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                race.getStages().get(i).go(this);
+            }
+            if (i == race.getStages().size() - 1) {
+                if (finishEventCount.incrementAndGet() == 1) {
+                    System.out.println(name + " - WIN");
+                } else if (finishEventCount.get() == CARS_COUNT) {
+                    System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+                }
+            }
         }
     }
 }
--- src/main/java/gb/MainClass.java
+++ src/main/java/gb/MainClass.java
@@ -1,19 +1,44 @@
 package gb;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+* 1. Организуем гонки:
+     Все участники должны стартовать одновременно, несмотря на то, что на подготовку у каждого из них уходит разное время.
+     В туннель не может заехать одновременно больше половины участников (условность).
+     Попробуйте всё это синхронизировать.
+     Только после того как все завершат гонку, нужно выдать объявление об окончании.
+     Можете корректировать классы (в т.ч. конструктор машин) и добавлять объекты классов из пакета util.concurrent.
+* */
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier cyclicBarrierForStartRace = new CyclicBarrier(CARS_COUNT);
+        AtomicInteger startDeclaringCount = new AtomicInteger(0);
+        AtomicInteger finishEventCount = new AtomicInteger(0);
+        Semaphore semaphore = new Semaphore(CARS_COUNT / 2);
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+            cars[i] = new Car(
+                    race,
+                    20 + (int) (Math.random() * 10),
+                    cyclicBarrierForStartRace,
+                    startDeclaringCount,
+                    semaphore,
+                    finishEventCount
+            );
         }
+        ExecutorService pool = Executors.newFixedThreadPool(CARS_COUNT);
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            pool.submit(cars[i]);
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        pool.shutdown();
     }
 }
